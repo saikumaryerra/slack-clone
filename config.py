06dcce83f4cc1d72389e9299d4aec1e316619a1b
saikumar <yerra.sai@mountblue.io>
2019-07-05 17:57:50 +0530

    added none in elasticsearch environment variable

--- config.py
+++ config.py
@@ -3,7 +3,8 @@ class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI= os.environ.get('POSTGRES_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    ELASTICSEARCH_URL = os.environ.get('ELASTICSEARCH_URL')
+    ELASTICSEARCH_URL = os.environ.get('ELASTICSEARCH_URL') or 'none'
+    # or 'http://localhost:9200'
     CELERY_BROKER_URL='redis://localhost:6379/0'
     CELERY_BACKEND_URL='redis://localhost:6379/0'
     # CELERY_BROKER_URL=os.environ.get('CELERY_BROKER_URL')
