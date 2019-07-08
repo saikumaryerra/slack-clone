604f350de5677b5d6f16d663ff217b8a9e6d89ef
Ubuntu <ubuntu@ip-172-31-39-110.ap-south-1.compute.internal>
2019-07-08 05:22:42 +0000

    added load_dot env to config

--- .flaskenv
+++ .flaskenv
@@ -1,6 +1,6 @@
 FLASK_APP=slack_clone.py
 FLASK_DEBUG=True
-ELASTICSEARCH_URL=http://localhost:9200
+
 POSTGRES_URL=postgresql://postgres:test123@localhost/slack_clone
 CELERY_BROKER_URL=redis://localhost:6379/0
 CELERY_BACKEND_URL=redis://localhost:6379/0
--- config.py
+++ config.py
@@ -1,4 +1,7 @@
 import os
+from dotenv import load_dotenv
+basedir = os.path.abspath(os.path.dirname(__file__))
+load_dotenv(os.path.join(basedir,'.flaskenv'))
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI= os.environ.get('POSTGRES_URL')
